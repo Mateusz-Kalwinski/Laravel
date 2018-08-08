19617dc1a787dd005b2cadaa1ce89e7fbad6eed9
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-08 12:28:25 +0200

    example doctors array

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    public function witajSwiecie(){
-        echo 'Witaj świecie';
+    public function index(){
+        $doctorsList =  array(
+            array("id"=>1, "lastName"=>"Newman", "firstName"=>"John", "email"=>"john@newman.com", "phone"=>"123123123", "address"=>"Adress 1", "status"=>"Dostępny"),
+            array("id"=>2, "lastName"=>"Austin", "firstName"=>"Adam", "email"=>"adam@austin.com", "phone"=>"456456456", "address"=>"Adress 2", "status"=>"Dostępny"),
+            array("id"=>3, "lastName"=>"Carrson", "firstName"=>"bob", "email"=>"bob@carrson.com", "phone"=>"789789789", "address"=>"Adress 3", "status"=>"Niedostępny"),
+        );
+        return view('doctors.list', ['doctorsList'=>$doctorsList]);
     }
 }
