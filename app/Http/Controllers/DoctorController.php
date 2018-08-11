0f659ff63b3e09ca959f050005de9eb1f9d5cfd7
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-11 20:50:43 +0200

    models added and get doctors from database

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -3,16 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DoctorController extends Controller
 {
     public function index(){
-        $doctorsList =  array(
-            array("id"=>1, "lastName"=>"Newman", "firstName"=>"John", "email"=>"john@newman.com", "phone"=>"123123123", "address"=>"Adress 1", "status"=>"Dostępny"),
-            array("id"=>2, "lastName"=>"Austin", "firstName"=>"Adam", "email"=>"adam@austin.com", "phone"=>"456456456", "address"=>"Adress 2", "status"=>"Dostępny"),
-            array("id"=>3, "lastName"=>"Carrson", "firstName"=>"bob", "email"=>"bob@carrson.com", "phone"=>"789789789", "address"=>"Adress 3", "status"=>"Niedostępny"),
-        );
-        return view('doctors.list', ['doctorsList'=>$doctorsList,
+
+        $users = User::where('type', 'doctor')->orderBy('name','asc')->get();
+
+        return view('doctors.list', ['doctorsList'=>$users,
                                           'footerYear'=>date('Y'),
                                           'title'=>'Moduł lekarzy'  ]);
     }
