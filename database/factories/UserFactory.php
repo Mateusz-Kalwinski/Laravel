a12ebc5c9175dc16075b9913d3bd49be378b3311
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-11 18:47:31 +0200

    factory for users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,8 +16,13 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'email' => $faker->email,
+        'password' => bcrypt('password'),
         'remember_token' => str_random(10),
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'status' => 'active',
+        'pesel' => $faker->ean13,
+        'type' => ''
     ];
 });
