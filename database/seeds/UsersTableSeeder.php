0544bc1ed24fc5011b39617ee15b9d924a7929df
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-11 18:49:21 +0200

    seed for users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        factory('App\User', 10)->create();
+
+        //specializations
+
+        DB::table('specializations')->insert([
+            'name'=>'oncology'
+        ]);
+
+        DB::table('specializations')->insert([
+            'name'=>'surgeon'
+        ]);
+
+        DB::table('specializations')->insert([
+            'name'=>'internist'
+        ]);
     }
 }
