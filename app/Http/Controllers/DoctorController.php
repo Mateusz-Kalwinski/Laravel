4085b55f728b5e48669a6ea5286fcbd58f0482a9
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-12 09:15:04 +0200

    show method. get single doctor from database

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -9,10 +9,19 @@ class DoctorController extends Controller
 {
     public function index(){
 
-        $users = User::where('type', 'doctor')->orderBy('name','asc')->get();
+        $doctors = User::where('type', 'doctor')->orderBy('name','asc')->get();
 
-        return view('doctors.list', ['doctorsList'=>$users,
+        return view('doctors.list', ['doctorsList'=>$doctors,
                                           'footerYear'=>date('Y'),
                                           'title'=>'Moduł lekarzy'  ]);
+    }
+
+    public function show($id){
+
+        $doctor =  User::find($id);
+
+        return view('doctors.show', ['doctor'=>$doctor,
+            'footerYear'=>date('Y'),
+            'title'=>'Profil lekarza'  ]);
     }
 }
