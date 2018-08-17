1742240240ca8dd90a5539c7043bdc2135e5904f
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-17 11:11:30 +0200

    statistics active from db

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,6 +4,7 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use DB;
 
 class UserRepository extends BaseRepository {
 
@@ -15,6 +16,12 @@ class UserRepository extends BaseRepository {
     }
     public function getAllDoctors(){
         return $this->model->where('type', 'doctor')->orderBy('name', 'asc')->get();
+    }
+
+    public function getDoctorsStatistics(){
+
+        return DB::table('users')->select(DB::raw('count(*) as user_count, status'))->where('type', 'doctor')->groupBy('status')->get();
+
     }
 
 }
