9e264398647eefbae97e13f91fe07e3954ce88a6
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-23 12:19:12 +0200

    views list patients and single profile

---
+++ resources/views/patients/show.blade.php
@@ -0,0 +1,40 @@
+@extends('template')
+@section('title')
+    @if(isset($title))
+        - {{$title}}
+    @endif
+@endsection('title')
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                {{$patient->name}}
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <tr>
+                        <td>Name:</td>
+                        <td>{{$patient->name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td>{{$patient->email}}</td>
+                    </tr>
+                    <tr>
+                        <td>Phone:</td>
+                        <td>{{$patient->phone}}</td>
+                    </tr>
+                    <tr>
+                        <td>Adres:</td>
+                        <td>{{$patient->address}}</td>
+                    </tr>
+                    <tr>
+                        <td>Pesel:</td>
+                        <td>{{$patient->pesel}}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection('content')
