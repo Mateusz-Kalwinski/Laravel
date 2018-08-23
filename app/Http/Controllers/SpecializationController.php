fb8a885c946379fb8d37f8ea3fcca2e1867d2e48
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-23 13:20:41 +0200

    create and store method to save new specialization

--- app/Http/Controllers/SpecializationController.php
+++ app/Http/Controllers/SpecializationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Specialization;
 use Illuminate\Http\Request;
 use App\Repositories\SpecializationRepository;
 
@@ -14,5 +15,17 @@ class SpecializationController extends Controller
         return view('specializations.list', ['specializations'=>$spacializatoins,
                                                   'footerYear'=>date("Y"),
                                                   'title'=>'Moduł specjalizacji']);
+    }
+
+    public function create(){
+        return view('specializations.create', ['footerYear'=>date("Y")]);
+    }
+
+    public function store(Request $request){
+        $specialization = new Specialization;
+        $specialization->name = $request->input('name');
+        $specialization->save();
+
+        return redirect()->action('SpecializationController@index');
     }
 }
