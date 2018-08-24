7d06572cf8ab70dab4711ba8642dd64047cc9e87
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-24 11:47:44 +0200

    create and store method (add new visit)

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Visit;
 use Illuminate\Http\Request;
 use App\Repositories\VisitRepository;
+use App\Repositories\UserRepository;
 
 class VisitController extends Controller
 {
@@ -14,5 +16,28 @@ class VisitController extends Controller
         return view('visits.list', ['visits'=>$visits,
             'footerYear'=>date("Y"),
             'title'=>'Moduł wizyt']);
+    }
+
+    public function create(UserRepository $userRepo){
+
+        $doctors = $userRepo->getAllDoctors();
+        $patients = $userRepo->getAllPatient();
+
+        return view('visits.create', ['patients'=>$patients,
+                                           'doctors'=>$doctors,
+                                           'footerYear'=>date("Y"),
+                                           'title'=>'Moduł wizyt']);
+    }
+
+    public function store(Request $request){
+
+        $visit = new Visit;
+
+        $visit->doctor_id = $request->input('doctor');
+        $visit->patient_id= $request->input('patient');
+        $visit->date = $request->input('date');
+        $visit->save();
+
+        return redirect()->action('VisitController@index');
     }
 }
