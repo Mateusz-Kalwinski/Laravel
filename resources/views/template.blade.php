9332323164b7f730f6b4da0d3c1959287fc3ec5e
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-08-26 11:15:07 +0200

    menu for logged in and not logged in users

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -17,19 +17,51 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="{{URL::to('doctors')}}">Lekarze</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{URL::to('patients')}}">Pacjenci</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{URL::to('specializations')}}">Specializacje</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{URL::to('visits')}}">Wizyty</a>
-            </li>
+
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Logowanie') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Rejestracja') }}</a>
+                </li>
+            @else
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{URL::to('doctors')}}">Lekarze</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{URL::to('patients')}}">Pacjenci</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{URL::to('specializations')}}">Specializacje</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{URL::to('visits')}}">Wizyty</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Wyloguj') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
         </ul>
     </div>
 </nav>
@@ -39,7 +71,9 @@
 @yield('content')
 
 <footer class="text-center">
-    {{$footerYear}}
+    @if(isset($footerYear))
+        {{$footerYear}}
+    @endif
 </footer>
 
 {{--Przykład dodawanie pliku JS--}}
